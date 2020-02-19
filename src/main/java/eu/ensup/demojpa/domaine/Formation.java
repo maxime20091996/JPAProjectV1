c607f0fd0c8259ceb7ac27ef24fd39c5e59549a4
Maxime Chevalier <chevalierm96@gmail.com>
2020-02-19 12:46:09 +0100

    Add files via upload

---
+++ src/main/java/eu/ensup/demojpa/domaine/Formation.java
@@ -0,0 +1,36 @@
+package eu.ensup.demojpa.domaine;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Formation {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String theme;
+
+
+	// Getters et Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTheme() {
+		return theme;
+	}
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	
+	// Constructeur
+	public Formation(String theme) {
+		super();
+		this.theme = theme;
+	}
+}
